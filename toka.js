04644ad96a00045bac26bb2c3b00300974cd0d61
Jarkko Makkonen <jaapimakkonen13@gmail.com>
2022-09-25 19:49:37 +0300

    greetings from function works

--- toka.js
+++ toka.js
@@ -2,7 +2,7 @@ function sayHello(){
     console.log("hello from function");
 }
 function greetings(fname){
-    constole.log("hello "+fname);
+    console.log("hello "+fname);
 }
 sayHello();
 greetings("Jussi");
